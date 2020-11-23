896221369d01d4c3a62852456c27d69c00adc4bb
coding_bot <68002888+DataSC3@users.noreply.github.com>
2020-11-23 06:11:24 +0300

    Update No-BlackMail.py

--- No-BlackMail.py
+++ No-BlackMail.py
@@ -3,7 +3,7 @@ import os
 RESET ='\033[0m'
 GREEN = '\033[32m'
 RED ='\033[31m'
-
+YELLOW = '\033[93m'
 
 if os.sys.platform == "win32":
     os.system("cls")
@@ -13,14 +13,10 @@ else:
 print(RED+'''█▄▄ █   █▀█ █▀▀ █▄▀ █▀▄▀█ █▀█ █ █'''+GREEN+'''
 █▄█ █▄▄ █▀█ █▄▄ █ █ █ ▀ █ █▀█ █ █▄▄'''+RESET+RED+'V: 0.0.0\n')
 
-print(f'{GREEN}-------------------{RESET}')
-
-print(f'{RESET}Мы закрыты!, чтобы узнать больше о проблеме, напишите нам в {GREEN}Telegram: FELIX4{RESET}')
-print(f'Для большего кол-во данных воспользуйтесь нашим {GREEN}Telegram Ботом: https://t.me/No_BlackMail_bot{RESET}\n')
-
+print(f'{RESET}-------------------')
+print(f'{RED}[~]{YELLOW} Мы закрыты!, чтобы узнать больше о проблеме, напишите нам в {GREEN}Telegram: FELIX4{RESET}')
+print(f'{RED}[~]{YELLOW} Для большего кол-во данных воспользуйтесь нашим {GREEN}Telegram Ботом: https://t.me/No_BlackMail_bot{RESET}')
 print(f'{RESET}-----------------------{RESET}')
-
 print(f'{RED}[+]{GREEN} Пробив по номеру\n{RED}[+]{GREEN} Номер по ФИО\n{RED}[+]{GREEN} Объявления/Авито\n{RED}[+]{GREEN} Пробив авто/UA\n{RED}[+]{GREEN} Пробив авто/RU (Фото)\n{RED}[+]{GREEN} Поиск по ФИО/RU\n{RED}[+]{GREEN} Поиск по VK{RESET}')
-
 print(f'{RESET}---------------------------{RESET}')
-print(f'{GREEN}Cпасибо что вы с нами!!{RESET}')
+print(f'{RED}[#]{YELLOW}Cпасибо что вы с нами!!{RESET}')
