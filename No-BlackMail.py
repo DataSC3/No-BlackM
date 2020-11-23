e51b8bb58304d3aef28e752a26a3fe3d89276b29
coding_bot <68002888+DataSC3@users.noreply.github.com>
2020-11-23 06:06:23 +0300

    Update No-BlackMail.py

--- No-BlackMail.py
+++ No-BlackMail.py
@@ -15,10 +15,10 @@ else:
     os.system("clear")
 
 print(banner())
-print(f'{RESET}_________________{RESET}')
+print(f'{GREEN}_________________{RESET}')
 print(f'{RESET}Мы закрыты!, чтобы узнать больше о проблеме, напишите нам в {GREEN}Telegram: FELIX4{RESET}')
 print(f'Для большего кол-во данных воспользуйтесь нашим {GREEN}Telegram Ботом: https://t.me/No_BlackMail_bot{RESET}\n')
 print(f'{RESET}__________________________{RESET}')
-print(f'{RED}+{GREEN} Пробив по номеру\n{RED}+{GREEN} Номер по ФИО\n{RED}+{GREEN} Объявления/Авито\n{RED}+{GREEN} Пробив авто/UA\n{RED}+{GREEN} Пробив авто/RU (Фото)\n{RED}+{GREEN} Поиск по ФИО/RU\n{RED}+{GREEN} Поиск по VK{RESET}')
+print(f'{RED}[+]{GREEN} Пробив по номеру\n{RED}[+]{GREEN} Номер по ФИО\n{RED}[+]{GREEN} Объявления/Авито\n{RED}[+]{GREEN} Пробив авто/UA\n{RED}[+]{GREEN} Пробив авто/RU (Фото)\n{RED}[+]{GREEN} Поиск по ФИО/RU\n{RED}[+]{GREEN} Поиск по VK{RESET}')
 print(f'{RESET}________________________________{RESET}')
 print(f'{GREEN}Cпасибо что вы с нами!!{RESET}')
