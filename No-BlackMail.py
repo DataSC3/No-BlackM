81c1e99ea76c54213ccfe2b46193bb98b07af147
coding_bot <68002888+DataSC3@users.noreply.github.com>
2024-10-09 07:42:45 +0400

    Update No-BlackMail.py

--- No-BlackMail.py
+++ No-BlackMail.py
@@ -14,7 +14,7 @@ else:
 print(RED+'''█▄▄ █   █▀█ █▀▀ █▄▀ █▀▄▀█ █▀█ █ █'''+GREEN+'''
 █▄█ █▄▄ █▀█ █▄▄ █ █ █ ▀ █ █▀█ █ █▄▄\n1.0.4'''+RESET+'\n')
 
-print(f'{RED}[!] {YELLOW}Мы выпустили новую версию 1.0.5. {GREEN}Эта уже не актуально.{RESET}')
+print(f'{RED}[!] {YELLOW}Мы выпустили новую версию 1.0.7. {GREEN}Эта уже не актуально.{RESET}')
 print(f'{RED}[+] {YELLOW}Ссылка на новую версию: {GREEN}https://github.com/DataSC3/noblack-mail.git{RESET}')
 print(f'{RED}[+] {YELLOW}Ссылка на наш канал в телеграмме: {GREEN}https://t.me/noblack_channel{RESET}')
 print(f'{RED}[#] {YELLOW}Cпасибо что вы с нами!!{RESET}\n\n')
